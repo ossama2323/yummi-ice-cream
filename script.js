38d75b170be30f79d2170ebeacac050d0e4b3866
ossama argaz <ossama2323>
2024-12-23 22:02:07 +0100

    firt commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+$('.testimonials-container').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout: 3500,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+            "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        }
+    }
+})
